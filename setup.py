5c7efe5b1f7c696afb90575e1659af8526380170
EEKIM10 <eek@clicksminuteper.net>
2021-01-16 14:18:24 +0000

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='custom.py',
+    version='1.0.0',
+    packages=['dds.custom'],
+    url='https://github.com/dragdev-studios/custom.py',
+    license='MIT',
+    author='EEKIM10',
+    author_email='eek@clicksminuteper.net',
+    description='Just a collection DDS bots commonly use cross-bots. Nothing fancy.'
+)
